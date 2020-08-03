5655f2b3a820761b2cf788b128f2b021b2ae3f8d
unknown <bruna.yoshie@iventis.com.br>
2020-08-02 22:06:33 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercicio_10__Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Console.WriteLine("Digite um valor para A");
+
+                var a = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite um valor para B");
+
+                var b = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite um valor para C");
+
+                var c = Convert.ToDouble(Console.ReadLine());
+
+                var resultado = VerificarSeEhTriangulo(a, b, c);
+
+                Console.WriteLine(resultado);
+
+                Console.ReadKey();
+            }
+
+            static string VerificarSeEhTriangulo(double a, double b, double c)
+            {
+                //https://pt.calcprofi.com/calculadora-triangle.html
+                if ((a < (b + c) && b < (a + c) && c < (a + b)))
+                {
+                    return "É um triângulo";
+                }
+                else
+                {
+                    return "NÃO é um triângulo";
+                }
+            }
+        }
+    }
+}
